b8eb36a4671d95c95b39b58bd5cb71060b44367f
Ekansh-dev <leoekansh@gmail.com>
2025-09-25 17:41:22 +0000

    container

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId= 1232
+let accountEmail="leo@gmail.com"
+var accountPassword="12345"
+accountCity="New York"
+
+console.log(accountId)
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity])
